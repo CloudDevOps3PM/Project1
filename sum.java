e622589cd223a9d1688b23e5f42774ab2f64f774
ravi <ravindra.devops@gmail.com>
2021-08-25 11:25:36 +0530

    sum.java V1.1

--- sum.java
+++ sum.java
@@ -3,7 +3,7 @@ class sum
 	public static void main(String[] args)
 	{
 		int a=60,b=500,c=600;c=a+b+c;
-		System.ouprintln("The sum of three values are "+c);
+		System.out.println("The sum of three values are "+c);
 	}
 }
 
